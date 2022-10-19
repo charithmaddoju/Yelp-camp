60783a83604a4bfdbf08bab9ca9ba97ec09c51a9
Charith <charithmaddoju27@gmail.com>
2022-10-19 10:16:28 +0530

    Add review model

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const Joi = require('joi')
-const {campgroundSchema} = require('./schemas')
+const {campgroundSchema,reviewSchema} = require('./schemas')
 const Campground = require('./models/campground')
 const { validate } = require('./models/campground')
+const Review = require('./models/review')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
     .then(() => {
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
         console.log(err)
     })
     
+
 
 const app = express()
 
@@ -37,6 +39,22 @@ const validateCampground = (req,res,next) => {
         const msg = error.details.map(el => el.message).join(',') 
         throw new ExpressError(msg,400)
     }
+    else{
+        next()
+    }
+}
+
+const validateReview = (req,res,next) => {
+
+    const {error} = reviewSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(',') 
+        throw new ExpressError(msg,400)
+    }
+    else{
+        next()
+    }
+
 }
 
 
@@ -66,7 +84,7 @@ app.post('/campgrounds', validateCampground ,catchAsync(async (req,res,next) => 
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews')
     res.render('campgrounds/show',{campground})
 }))
 
@@ -85,6 +103,23 @@ app.delete('/campgrounds/:id', catchAsync(async(req,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview ,catchAsync(async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req,res) => {
+    const {id,reviewId} = req.params
+    await Campground.findByIdAndUpdate(id,{$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
 }))
 
 app.all('*',(req,res,next) => {
--- schemas.js
+++ schemas.js
@@ -1,4 +1,6 @@
 const Joi = require('joi')
+const { number } = require('joi');
+
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
@@ -7,5 +9,12 @@ module.exports.campgroundSchema = Joi.object({
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
+    }).required()
+})
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        body: Joi.string().required(),
+        rating: Joi.number().required().min(1).max(5),
     }).required()
 })
